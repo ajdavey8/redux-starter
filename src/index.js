560eb3bd0d211b69cb097489e3270c31a08efb79
Andrew Davey <ajdavey8@gmail.com>
2018-03-06 23:05:28 +0000

    make app a class and create api call for videos to state

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/searchBar';
 import YTSearch from'youtube-api-search';
@@ -6,16 +6,26 @@ import {config} from 'dotenv';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-YTSearch({key:API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
 
-const App = () => {
- return (
-   <div>
-   <SearchBar />
-    </div>
-  )
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key:API_KEY, term: 'surfboards'}, (data) => {
+      this.setState({ videos:data });
+    });
+
+  }
+  render() {
+   return (
+     <div>
+     <SearchBar />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
