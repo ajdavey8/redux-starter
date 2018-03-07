7244dfdc3a1b918a7af2134309c8d5becd0471ea
Andrew Davey <ajdavey8@gmail.com>
2018-03-07 23:21:49 +0000

    Add search functionality and throttle input

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import SearchBar from './components/searchBar';
 import VideoList from './components/VideoList';
 import VideoDetail from './components/videoDetails';
 import YTSearch from'youtube-api-search';
+import _ from 'lodash';
 import {config} from 'dotenv';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
@@ -19,7 +20,11 @@ class App extends Component {
       selectedVideo: null
      };
 
-    YTSearch({key:API_KEY, term: 'micky flanagan'}, (videos) => {
+    this.videoSearch('micky flanagan')
+  }
+
+  videoSearch(term) {
+    YTSearch({key:API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -28,9 +33,11 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch= _.debounce((term) =>{this.videoSearch(term)}, 300);
+
    return (
       <div>
-      <SearchBar />
+      <SearchBar onSearchTermChange={videoSearch}/>
       <VideoDetail video={this.state.selectedVideo} />
       <VideoList
         onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
